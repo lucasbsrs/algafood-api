574b56e90c0efa4df7bf7deb1ae2982f6382616e
Lucas Batista da Silva <lucas.batista@paipe.co>
2020-12-04 20:45:32 -0300

    Desafio JPA

---
+++ src/main/java/com/algaworks/algafood/domain/model/Estado.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Estado {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+}
