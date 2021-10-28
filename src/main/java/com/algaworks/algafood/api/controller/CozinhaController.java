a22f86d26bfdb73f2f1f1b52afb766e416dac934
Lucas Batista da Silva <lucas.batista@paipe.co>
2021-10-27 22:37:56 -0300

    Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -26,9 +26,6 @@ public class CozinhaController {
 
     @GetMapping
     public List<Cozinha> listar() {
-        for (int i = 0; i < 100000000; i++) {
-            System.out.println(i);
-        }
         return repository.findAll();
     }
 
